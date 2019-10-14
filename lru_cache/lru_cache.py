5683698247cb5f4697177386a9be1087f976fa97
Nolanole <Jmancuso82@gmail.com>
2019-10-14 18:11:06 -0500

    added Queues and stacks

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,5 @@
+from doubly_linked_list import ListNode, DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +9,7 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.size = 
 
     """
     Retrieves the value associated with the given key. Also
--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,26 @@
 import sys
 sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import DoublyLinkedList, ListNode
 
 
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_head(value)
+        self.size = self.storage.length
 
     def dequeue(self):
-        pass
+        if self.size == 0:
+            return
+        else:
+            val = self.storage.tail.value
+            self.storage.remove_from_tail()
+            self.size = self.storage.length
+            return val
 
     def len(self):
-        pass
+        return self.size
